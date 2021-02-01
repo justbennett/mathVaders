98c073997a5714d1ec7c22398dcddcde12a6451e
justbennett <justbennett@mail.com>
2021-02-01 07:27:48 -0600

    Update mathVaders.js

--- mathVaders.js
+++ mathVaders.js
@@ -67,7 +67,7 @@ function setup(){
 	createCanvas(windowWidth, windowHeight);
 	//canv.parent("title");
 	link = createA("..", "Go Back");
-	link.position(10, height-10);
+	link.position(10, height-30);
 	sine = new p5.Oscillator();
 	sine.setType('sine');
 	noise = new p5.Oscillator();
